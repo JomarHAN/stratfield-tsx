40ff3650519be3b4e54eefab975d26d988cd3a2e
Jomar <jomarhan.ng@gmail.com>
2024-08-12 05:59:33 -0400

    updated faq

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -47,8 +47,10 @@ const FAQCompoent = (props: Props) => {
           <div className="faq-accordion">
             <div className="faq-accordion-item">
               <div className="faq-accordion-title">
-                <img src="home-work-icon.svg" alt="icon" />
-                <h2>Consulting</h2>
+                <div className="faq-accordion__icon-box">
+                  <img src="marketing.svg" alt="icon" />
+                </div>
+                <h2>Marketing</h2>
               </div>
               <div className="faq-accordion-content">
                 <p>
@@ -68,8 +70,10 @@ const FAQCompoent = (props: Props) => {
           <div className="faq-accordion">
             <div className="faq-accordion-item">
               <div className="faq-accordion-title">
-                <img src="home-work-icon.svg" alt="icon" />
-                <h2>Consulting</h2>
+                <div className="faq-accordion__icon-box">
+                  <img src="change.svg" alt="icon" />
+                </div>
+                <h2>Change Management</h2>
               </div>
               <div className="faq-accordion-content">
                 <p>
@@ -89,8 +93,56 @@ const FAQCompoent = (props: Props) => {
           <div className="faq-accordion">
             <div className="faq-accordion-item">
               <div className="faq-accordion-title">
-                <img src="home-work-icon.svg" alt="icon" />
-                <h2>Consulting</h2>
+                <div className="faq-accordion__icon-box">
+                  <img src="technology.svg" alt="icon" />
+                </div>
+                <h2>Technology Enablement</h2>
+              </div>
+              <div className="faq-accordion-content">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged.{" "}
+                </p>
+                <button className="read-more-button">Read more</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="faq-accordion">
+            <div className="faq-accordion-item">
+              <div className="faq-accordion-title">
+                <div className="faq-accordion__icon-box">
+                  <img src="software.svg" alt="icon" />
+                </div>
+                <h2>Software Engineering</h2>
+              </div>
+              <div className="faq-accordion-content">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged.{" "}
+                </p>
+                <button className="read-more-button">Read more</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="faq-accordion">
+            <div className="faq-accordion-item">
+              <div className="faq-accordion-title">
+                <div className="faq-accordion__icon-box">
+                  <img src="project.svg" alt="icon" />
+                </div>
+                <h2>Project Management</h2>
               </div>
               <div className="faq-accordion-content">
                 <p>
